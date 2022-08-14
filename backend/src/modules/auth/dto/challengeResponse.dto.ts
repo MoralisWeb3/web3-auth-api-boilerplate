c6a9d58044d1579371feb21a287a78689663f5da
Vincent Hoong <vincent@moralis.io>
2022-08-14 20:51:13 +0800

    Update content

--- backend/src/modules/auth/dto/challengeResponse.dto.ts
+++ backend/src/modules/auth/dto/challengeResponse.dto.ts
@@ -18,7 +18,7 @@ export class ChallengeResponseDto {
     required: true,
     description: 'Message that needs to be signed by the end user',
     example:
-      'rugpull.finance wants you to sign in with your Ethereum account:\n0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B\n\nPlease confirm\n\nURI: https://rugpull.finance/\nVersion: 1\nChain ID: 1\nNonce: DbU1DCTmdzR4lg3wi\nIssued At: 2022-06-12T12:15:31.290Z\nExpiration Time: 2020-01-01T00:00:00.000Z\nNot Before: 2020-01-01T00:00:00.000Z\nResources:\n- https://docs.moralis.io/',
+      'defi.finance wants you to sign in with your Ethereum account:\n0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B\n\nPlease confirm\n\nURI: https://defi.finance/\nVersion: 1\nChain ID: 1\nNonce: DbU1DCTmdzR4lg3wi\nIssued At: 2022-06-12T12:15:31.290Z\nExpiration Time: 2020-01-01T00:00:00.000Z\nNot Before: 2020-01-01T00:00:00.000Z\nResources:\n- https://docs.moralis.io/',
   })
   message: string;
 
